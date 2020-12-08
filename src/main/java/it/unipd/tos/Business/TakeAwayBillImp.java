a0d2b7cef18321d7a0b4c38eff35c1f28863d3ad
Ubuntu <ubuntu@ubuntu1804.linuxvmimages.com>
2020-12-09 00:09:08 +0100

    close #2

--- src/main/java/it/unipd/tos/Business/TakeAwayBillImp.java
+++ src/main/java/it/unipd/tos/Business/TakeAwayBillImp.java
@@ -6,17 +6,40 @@ package it.unipd.tos.business;
 
 import java.util.List;
 import it.unipd.tos.business.exception.RestaurantBillException;
+import it.unipd.tos.model.ItemType;
 import it.unipd.tos.model.MenuItem;
 import it.unipd.tos.model.User;
 
 public class TakeAwayBillImp implements TakeAwayBill {
-    public double getOrderPrice(List<MenuItem> itemsOrdered, User user)
-            throws RestaurantBillException {
+  public double getOrderPrice(List<MenuItem> itemsOrdered, User user)
+  throws RestaurantBillException {
 
-        double tot=0;
-        for (int i = 0; i < itemsOrdered.size(); i++) {
-            tot+=itemsOrdered.get(i).getPrice();
-        }
-        return tot;
+    int numeroGelati=0;
+    for(int i = 0; i < itemsOrdered.size(); i++) {
+      if(itemsOrdered.get(i).getItemType()==ItemType.Gelati) {
+        numeroGelati++;
+      }
     }
+
+    double tot=0;
+    double prezzoMinimo=0;
+
+    for (int i = 0; i < itemsOrdered.size(); i++) {
+
+      tot+=itemsOrdered.get(i).getPrice();
+
+      if(itemsOrdered.get(i).getItemType()==ItemType.Gelati) {
+        if( prezzoMinimo==0 ||
+        itemsOrdered.get(i).getPrice()<prezzoMinimo){
+          prezzoMinimo=itemsOrdered.get(i).getPrice();
+        }
+      }
+    }
+
+    if(numeroGelati>5) {
+      tot=tot-(prezzoMinimo/2);
+    }
+
+    return tot;
+  }
 }
--- src/test/java/it/unipd/tos/Business/TakeAwayBillTest.java
+++ src/test/java/it/unipd/tos/Business/TakeAwayBillTest.java
@@ -48,7 +48,7 @@ public class TakeAwayBillTest {
         List<MenuItem> BlankList = new ArrayList<MenuItem>();
 
         try {
-            assertEquals(0, test.getOrderPrice(BlankList, new User("12345","Vittorio","Schiavon",LocalDate.of(1999,12,21))),0);
+            assertEquals(0, test.getOrderPrice( BlankList , new User("12345","Vittorio","Schiavon",LocalDate.of(1999,12,21))),0);
         } catch (RestaurantBillException exce) {
             // TODO Auto-generated catch block
             fail("errore");
@@ -56,6 +56,49 @@ public class TakeAwayBillTest {
         }
 
     }
+
+    @Test
+    public void testSconto() {
+    List<MenuItem> ordine = new ArrayList<MenuItem>();
+    ordine.add(new MenuItem(ItemType.Gelati,"Vaniglia",4));
+    ordine.add(new MenuItem(ItemType.Gelati,"Cioccolato",5));
+    ordine.add(new MenuItem(ItemType.Gelati,"Pistacchio",6));
+    ordine.add(new MenuItem(ItemType.Gelati,"Banana",7));
+    ordine.add(new MenuItem(ItemType.Gelati,"Mirtillo",8));
+    ordine.add(new MenuItem(ItemType.Budini,"CoppaBudino",9));
+    ordine.add(new MenuItem(ItemType.Gelati,"Cassata",9));
+
+    try {
+        assertEquals(46, test.getOrderPrice(ordine, new User("12345","Vittorio","Schiavon",LocalDate.of(1999,12,21))),0);
+    } catch (RestaurantBillException exce) {
+        // TODO Auto-generated catch block
+        fail("errore");
+
+    }
+
+
+}
+
+    @Test
+    public void testNonSconto() {
+    List<MenuItem> ordine = new ArrayList<MenuItem>();
+    ordine.add(new MenuItem(ItemType.Gelati,"Vaniglia",4));
+    ordine.add(new MenuItem(ItemType.Gelati,"Cioccolato",5));
+    ordine.add(new MenuItem(ItemType.Gelati,"Pistacchio",6));
+    ordine.add(new MenuItem(ItemType.Gelati,"Banana",7));
+    ordine.add(new MenuItem(ItemType.Gelati,"Mirtillo",8));
+    ordine.add(new MenuItem(ItemType.Budini,"CoppaBudino",8));
+
+    try {
+        assertEquals(38, test.getOrderPrice(ordine, new User("12345","Vittorio","Schiavon",LocalDate.of(1999,12,21))),0);
+    } catch (RestaurantBillException exce) {
+        // TODO Auto-generated catch block
+        fail("errore");
+
+    }
+
+
+  }
 
 
 }
